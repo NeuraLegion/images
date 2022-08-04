bb3ab394deb2617f83a28648761786a88153606d
Alexander Kutsan <alexkutsan@gmail.com>
2022-08-04 13:45:14 +0300

    startup works

--- akutsan/run_chromium.sh
+++ akutsan/run_chromium.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ip=$(hostname --ip-address)
+nohup socat tcp-listen:9222,bind="$ip",fork tcp:127.0.0.1:9222 &
 (ulimit -n 65000 || true) && (ulimit -p 65000 || true) && nohup /usr/bin/chromium-browser \
-  --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --disable-gpu --no-sandbox\
+  --remote-debugging-port=9222 --disable-gpu --no-sandbox\
   "$@" &
