1909d81669c314df3a06d7a62f6123d3bfeb492e
Alexander Kutsan <alexkutsan@gmail.com>
2022-08-04 12:45:28 +0300

    debug

--- akutsan/run_chromium.sh
+++ akutsan/run_chromium.sh
@@ -5,31 +5,5 @@ if [ "$(ls -A /home/chromium/.fonts/)" ]; then
 fi
 
 (ulimit -n 65000 || true) && (ulimit -p 65000 || true) && /usr/bin/chromium-browser \
-  --disable-background-networking \
-  --disable-background-timer-throttling \
-  --disable-backgrounding-occluded-windows \
-  --disable-renderer-backgrounding \
-  --disable-breakpad \
-  --disable-client-side-phishing-detection \
-  --disable-default-apps \
-  --disable-dev-shm-usage \
-  --disable-extensions \
-  --disable-gpu \
-  --disable-popup-blocking \
-  --disable-prompt-on-repost \
-  --disable-sync \
-  --disable-translate \
-  --hide-scrollbars \
-  --ignore-certificate-errors \
-  --ignore-certificate-errors-spki-list \
-  --ignore-ssl-errors \
-  --metrics-recording-only \
-  --mute-audio \
-  --no-first-run \
-  --no-sandbox \
-  --remote-debugging-address=0.0.0.0 \
-  --remote-debugging-port="$RD_PORT" \
-  --safebrowsing-disable-auto-update \
-  --user-data-dir=/home/chromium/ \
-  --window-size=1920,1080 \
+  --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --headless --disable-gpu \
   "$@"
