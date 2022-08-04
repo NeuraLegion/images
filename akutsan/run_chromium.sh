760be8c3aa08820737d023a51d7980b98067b66f
Alexander Kutsan <alexkutsan@gmail.com>
2022-08-04 13:04:32 +0300

    autostart chromium

--- akutsan/run_chromium.sh
+++ akutsan/run_chromium.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "$(ls -A /home/chromium/.fonts/)" ]; then
-  fc-cache -f -v
-fi
-
-(ulimit -n 65000 || true) && (ulimit -p 65000 || true) && /usr/bin/chromium-browser \
-  --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --headless --disable-gpu \
-  "$@"
+(ulimit -n 65000 || true) && (ulimit -p 65000 || true) && nohup /usr/bin/chromium-browser \
+  --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --disable-gpu --no-sandbox\
+  "$@" &
